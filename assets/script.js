e356352810f96c605cb1419c8fc0a6b39416938e
ELiud <e.arbelo2@gmail.com>
2018-01-25 09:40:17 -0500

    changes to cart tabs

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,24 @@
 $(document).ready(function {
 
 	// checkout tab events
-	$('#tabOne').on('click', function (e) {
+	$('#tabOne').on('click', function(e) {
 	  e.preventDefault()
 	  $(this).tab('show')
-	})
+	});
 
-	$('#tabTwo').on('click', function (e) {
+	$('#tabTwo').on('click', function(e) {
 	  e.preventDefault()
 	  $(this).tab('show')
-	})
+	});
 
-	$('#tabThree').on('click', function (e) {
+	$('#tabThree').on('click', function(e) {
 	  e.preventDefault()
 	  $(this).tab('show')
-	})
+	});
+
+	$('#continueSB').on('submit',function() {
+		$('#tabOne').hide();
+		$('#tabTwo').show();		
+	});
 
 })
