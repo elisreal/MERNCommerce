9aae9068e1414cda58cd12456fc2f915d11f6c38
ELiud <e.arbelo2@gmail.com>
2018-01-24 21:14:37 -0500

    bootstrap views

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function {
+
+	// checkout tab events
+	$('#tabOne').on('click', function (e) {
+	  e.preventDefault()
+	  $(this).tab('show')
+	})
+
+	$('#tabTwo').on('click', function (e) {
+	  e.preventDefault()
+	  $(this).tab('show')
+	})
+
+	$('#tabThree').on('click', function (e) {
+	  e.preventDefault()
+	  $(this).tab('show')
+	})
+
+})
