d311097adabd28e9363d567098a604273b7e4822
ELiud <e.arbelo2@gmail.com>
2018-02-08 19:10:05 -0500

    css changes

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -4,22 +4,22 @@ import { ListItem } from "../List";
 
 const Product = ({ id, productName, photoUrl, price, description, size, totalQty, handleProductDelete }) => (
 <ListItem>
-	<div className='row'>
-		<div className='col s2'>
+	<div className='row aProduct'>
+		<div className='col-2'>
 			<div className='aImg'>
 				<img source={photoUrl} className='pPhoto' alt={productName} />
 			</div>
 		</div>
-		<div className='col s2'>
+		<div className='col-2'>
 			<p className='adminProd'>{productName}</p>
-			<p className='adminProd'>{price}</p>
-			<p className='adminProd'>{size}</p>
+			<p className='adminProd'>PRICE: {price}</p>
+			<p className='adminProd'>SIZE: {size}</p>
 		</div>
-		<div className='col s6'>
+		<div className='col-6'>
 			<p className='adminProd'>{description}</p>
 		</div>
-		<div className='col s2'>
-			<p className='adminProd qty'>{totalQty}</p>
+		<div className='col-2'>
+			<p className='adminProd qty'>QTY: {totalQty}</p>
 			<button onClick={() => handleProductDelete(id)} className="btn btn-primary">DELETE</button>
 		</div>
 	</div>
